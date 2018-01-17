d7f3022c020d23daa00419dc994c9fdff80c78e3
maxmudriak <maxim.mudryak@gmail.com>
2018-01-17 19:01:32 +0200

    first commit

---
+++ src/assets/js/app.js
@@ -0,0 +1,45 @@
+//=require jquery.min.js
+
+// to inlude bootstrap plugins add "=" sign below
+// require javascripts/bootstrap/*.*
+//=require javascripts/bootstrap.min.js
+
+//=require slick.min.js
+
+;(function($){
+
+	$(document).ready(function(){
+		var $slider = $('.ba-slider');
+
+		$slider.slick({
+		dots: true,
+		infinite: false,
+            arrows: false,
+            infinite: true,
+            autoplay: true
+		});
+	});
+
+})(jQuery);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
